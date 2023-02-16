5ef06ec88dbabce66c9d122639f839a95313a4e7
VandanaVinay <121236483+VandanaVinay@users.noreply.github.com>
2023-02-16 19:02:54 +0530

    Update Variable.tf

--- Variable.tf
+++ Variable.tf
@@ -17,3 +17,35 @@ variable "port" {
 variable "cidr_block" {
     default = "0.0.0.0/0"
 }
+
+
+variable "create_vpc" {
+  description = "Controls if VPC should be created (it affects almost all resources)"
+  type        = bool
+  default     = true
+}
+
+variable "bucket" {
+  description = "The name of the bucket. (forces new resource, default: unique random name)"
+  type        = string
+  default     = null
+}
+
+variable "bucket_prefix" {
+  description = "Creates a unique bucket name beginning with the specified prefix. Conflicts with bucket. (forces new resource)"
+  type        = string
+  default     = null
+}
+
+variable "acl" {
+  description = "The canned ACL to apply. https://docs.aws.amazon.com/AmazonS3/latest/dev/acl-overview.html#canned-acl"
+  type        = string
+  default     = "private"
+}    
+
+variable "versioning" {
+  description = "Boolean specifying enabled state of versioning or object containing detailed versioning configuration."
+  type        = any
+  default     = false
+}    
+    
